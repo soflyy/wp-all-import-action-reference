0139e9389596c0c9ed2279e074de129103880c93
Trey M <trey8611@gmail.com>
2016-12-30 17:56:24 -0500

    Added example use.

--- all-import/wp_all_import_feed_type.php
+++ all-import/wp_all_import_feed_type.php
@@ -13,6 +13,21 @@
 add_filter( 'wp_all_import_feed_type', 'wpai_feed_type', 10, 2 );
 
 function wpai_feed_type( $type, $url ){
+        // Check $url and return $type.
+}
+
+// ----------------------------
+// Example uses below
+// ----------------------------
+
+/**
+ * Define the type as XML.
+ *
+ *
+ */
+add_filter( 'wp_all_import_feed_type', 'wpai_feed_type', 10, 2 );
+
+function wpai_feed_type( $type, $url ){
         if ($url == 'https://www.example.com/feedurl'){
                 $type = 'xml';
         }
