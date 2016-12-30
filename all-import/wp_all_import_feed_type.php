0172c4077fb3e995a283a83861768f02c63cba43
Trey M <trey8611@gmail.com>
2016-12-30 17:52:56 -0500

    Added new hook wp_all_import_feed_type

---
+++ all-import/wp_all_import_feed_type.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ==================================
+ * Filter: wp_all_import_feed_type
+ * ==================================
+ *
+ * Can be used to define the feed type.
+ *
+ * @param $type string 	- The feed type ( 'xml' or 'csv', for example )
+ * @param $url string 	- The feed URL
+ */
+
+add_filter( 'wp_all_import_feed_type', 'wpai_feed_type', 10, 2 );
+
+function wpai_feed_type( $type, $url ){
+        if ($url == 'https://www.example.com/feedurl'){
+                $type = 'xml';
+        }
+        return $type;
+}
