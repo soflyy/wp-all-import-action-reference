899201103c03555c322777e2688e73180d470deb
Trey M <trey8611@gmail.com>
2016-12-30 18:02:16 -0500

    Minor change.

--- all-import/wp_all_import_is_unlink_missing_posts.php
+++ all-import/wp_all_import_is_unlink_missing_posts.php
@@ -7,8 +7,8 @@
  * Used to completely remove the relationship between import and post from pmxi_posts database table.
  *
  * @param $is_unlink_missing 	- ????
- * @param $import_id 			- ????
- * @param $post_id 				- ????
+ * @param $import_id 			- The Import ID.
+ * @param $post_id 				- The Post ID.
  */
 
 add_filter( 'wp_all_import_is_unlink_missing_posts', 'wpai_wp_all_import_is_unlink_missing_posts', 10, 3 );
