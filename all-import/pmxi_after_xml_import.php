58e1c0f05d6f608f273f5330d63e91d93b36455c
Trey M <trey8611@gmail.com>
2016-12-30 17:21:47 -0500

    Added example for after xml import.

--- all-import/pmxi_after_xml_import.php
+++ all-import/pmxi_after_xml_import.php
@@ -18,3 +18,24 @@ function after_xml_import($import_id)
 
 add_action('pmxi_after_xml_import', 'after_xml_import', 10, 1);
 
+
+// ----------------------------
+// Example uses below
+// ----------------------------
+/**
+ * Find the import file after the import completes.
+ *
+ *
+ */
+add_action( 'pmxi_after_xml_import', 'after_xml_import', 10, 1 );
+
+function after_xml_import( $import_id ) {
+    $file = new PMXI_File_Record();
+    $file->getBy( array('import_id' => $import_id), 'id DESC' );
+    $local_path = ( ! $file->isEmpty() ) ? wp_all_import_get_absolute_path($file->path) : false;
+    if ( ! empty($local_path)) {                                                           
+        if ( @file_exists($local_path) ){
+            // Do something with the file $local_path
+        }
+    }
+}
