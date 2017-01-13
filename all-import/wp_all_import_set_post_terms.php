7cdcf0d0819beaaaf1f9bc385efa9736c9e54568
Trey Mills <Trey Mills>
2017-01-13 15:51:20 -0500

    Added new hook wp_all_import_set_post_terms

---
+++ all-import/wp_all_import_set_post_terms.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * ==============================================
+ * Filter: wp_all_import_set_post_terms
+ * ==============================================
+ *
+ * Auto generate headers, or use header in file.
+ *
+ * @param $create_headers
+ * @param $import_id
+ *
+ * @return boolean
+ */
+add_filter( 'wp_all_import_auto_create_csv_headers', 'wpai_wp_all_import_auto_create_csv_headers', 10, 2 ); 
+
+function wpai_wp_all_import_auto_create_csv_headers( $create_headers, $import_id ){ 
+    // Return true to auto-generate header, or false to use the header in the file.
+}
+
+// ----------------------------
+// Example uses below
+// ----------------------------
+
+/**
+ * Example: Auto generate header
+ *
+ */
+add_filter( 'wp_all_import_auto_create_csv_headers', 'wpai_wp_all_import_auto_create_csv_headers', 10, 2 ); 
+
+function wpai_wp_all_import_auto_create_csv_headers( $create_headers, $import_id ){ 
+    return true; 
+}
