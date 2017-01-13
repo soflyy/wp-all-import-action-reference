344b24e3eec17a127d261609af20eb29b520022b
Trey Mills <Trey Mills>
2017-01-13 16:01:39 -0500

    Added hook wp_all_export_config_options

---
+++ all-export/wp_all_export_config_options.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * ======================================
+ * Filter: wp_all_export_config_options
+ * ======================================
+ * Create additional fields for export.
+ *
+ * @param $options array - export configuration options.
+ *
+ * @return array
+ */
+add_filter( 'wp_all_export_config_options', 'wpai_wp_all_import_config_options', 10, 1 );
+
+function wpai_wp_all_import_config_options( $options ) {
+	// Code here.
+}
+
+
+// ----------------------------
+// Example uses below
+// ----------------------------
+
+/**
+ * Example: set the 'max_execution_time' to 30 seconds.
+ *
+ */
+add_filter( 'wp_all_export_config_options', 'wpai_wp_all_import_config_options', 10, 1 );
+
+function wpai_wp_all_import_config_options( $options ) {
+	$options['max_execution_time'] = 30;
+	return $options;
+}
+
