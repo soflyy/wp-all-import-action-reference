b72c43b51ee85585d286a71e31bd5073d7bd5502
mbissett <czechdude489@gmail.com>
2017-07-18 21:49:47 -0500

    Create wp_all_export_product_variation_mode.php

---
+++ all-export/wp_all_export_product_variation_mode.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * ==============================================
+ * Filter: wp_all_export_product_variation_mode
+ * ==============================================
+ * Choose whether to export parent products or just variations.
+ *
+ *
+ * @param $exportVariationMode
+ * @param $exportID  int
+ *
+ */
+ 
+// Stop parent products from exporting for all exports
+
+add_filter('wp_all_export_product_variation_mode', 'export_only_variations', 10, 2);
+
+function export_only_variations($exportVariationMode, $exportID){
+	return XmlExportEngine::VARIABLE_PRODUCTS_EXPORT_VARIATION; // Only export variations for variable products, don't export the parent products
+}
+
+// Stop parent products from exporting for specific export
+
+add_filter('wp_all_export_product_variation_mode', 'export_only_variations_for_specific_export', 10, 2);
+
+function export_only_variations_for_specific_export($exportVariationMode, $exportID){
+	if($exportID && $exportID == 4) {
+		return XmlExportEngine::VARIABLE_PRODUCTS_EXPORT_VARIATION; // Only export variations for variable products, don't export the parent products
+	} else {
+		return $exportVariationMode;
+	}
+}
