ee4ebe2fdee383e80c5be6f3fc09c5b227301285
Mark Thomson <hello@markthomson.ca>
2017-09-20 18:31:42 -0700

    Make example check $import_id

--- all-import/wp_all_import_is_post_to_create.php
+++ all-import/wp_all_import_is_post_to_create.php
@@ -9,9 +9,8 @@
  * filter is for updating existing posts
  *
  * @param $data       array - An array holding values for the current record. If importing from
- *                                 XML, attributes can be accessed as SimpleXMLElement objects.
- * @param $import_id  int   - Import id. It can be used to make the code limited to a specific
- *                                 import only. 
+ *                            XML, attributes can be accessed as SimpleXMLElement objects.
+ * @param $import_id  int   - Import id. It can be used to make the code limited to a specific import only. 
  *
  * @return bool (true = create, false = skip)
  */
@@ -32,29 +31,27 @@ add_filter('wp_all_import_is_post_to_create', 'my_is_post_to_create', 10, 2);
 /**
  * Only allow creation of a new post if an existing custom field value
  * doesn't already exist.
- *
- * @param $data
- *
- * @return bool
  */
-function create_only_if_unique_custom_field($data)
+function create_only_if_unique_custom_field($data, $import_id)
 {
-    $key_to_look_up = "my_custom_field"; // Change this to your custom field name
-    $value_to_look_up = $data['num'];    // Change this to the column name
-
+    // Check for your import
+    if ($import_id == 1) {
+    	$key_to_look_up = "my_custom_field"; // Change this to your custom field name
+    	$value_to_look_up = $data['num'];    // Change this to the column name
 	$args = array (
 		'post_type'         => array( 'post' ),
-		'meta_query'        => array(
-			array(
-				'key'       => $key_to_look_up,
-				'value'     => $value_to_look_up,
-				'compare'   => '=',
-			),
-		),
+		'meta_query'        => array(array(
+			'key'       => $key_to_look_up,
+			'value'     => $value_to_look_up,
+		)),
 	);
 
 	$query = new WP_Query( $args );
 	return !($query->have_posts());
+    }
+    else {    
+       return true;
+    }
 }
 
 add_filter('wp_all_import_is_post_to_create', 'create_only_if_unique_custom_field', 10, 2);
