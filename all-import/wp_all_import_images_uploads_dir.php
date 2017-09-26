bcea7ddae5b9ecd67782988a9499c717046099d7
mbissett <czechdude489@gmail.com>
2017-09-26 02:52:46 -0500

    Updated example snippets

--- all-import/wp_all_import_images_uploads_dir.php
+++ all-import/wp_all_import_images_uploads_dir.php
@@ -29,12 +29,42 @@ function wpai_wp_all_import_images_uploads_dir($uploads, $articleData, $current_
 // ----------------------------
 
 
-add_filter('wp_all_import_images_uploads_dir', 'wpai_wp_all_import_images_uploads_dir', 10, 4);
+/**
+ * Upload all images for the post to a folder called "customfolder"
+ *
+ */
 
-function wpai_wp_all_import_images_uploads_dir($uploads, $articleData, $current_xml_node, $import_id){
-    if ( ! empty($articleData['post_date'])){
-        $uploads['path'] = $uploads['basedir'] . '/' . date("Y/m", strtotime($articleData['post_date']));
-        $uploads['url'] = $uploads['baseurl'] . '/' . date("Y/m", strtotime($articleData['post_date']));
-    }
-    return $uploads;
+function wpai_set_custom_upload_folder($uploads, $articleData, $current_xml_node, $import_id) {
+	$uploads['path'] = $uploads['basedir'] . '/customfolder';
+	$uploads['url'] = $uploads['baseurl'] . '/customfolder';    
+
+	if (!file_exists($uploads['path'])) {
+		mkdir($uploads['path'], 0755, true);
+	}
+
+	return $uploads;
 }
+
+add_filter('wp_all_import_images_uploads_dir', 'wpai_set_custom_upload_folder', 10, 4);
+
+/**
+ * Upload all images for the post to a folder based on the post date (in Y/m format)
+ *
+ * (e.g. if the post was published on June 1st 2017, its images would
+ *  be uploaded to /wp-content/uploads/2017/06)
+ *
+ */
+ 
+function wpai_set_upload_folder_by_post_date($uploads, $articleData, $current_xml_node, $import_id) {
+	if ( ! empty($articleData['post_date'])) {
+		$uploads['path'] = $uploads['basedir'] . '/' . date("Y/m", strtotime($articleData['post_date']));
+		$uploads['url'] = $uploads['baseurl'] . '/' . date("Y/m", strtotime($articleData['post_date']));
+		
+		if (!file_exists($uploads['path'])) {
+			mkdir($uploads['path'], 0755, true);
+		}
+	}
+	return $uploads;
+}
+
+add_filter('wp_all_import_images_uploads_dir', 'wpai_set_upload_folder_by_post_date', 10, 4);
