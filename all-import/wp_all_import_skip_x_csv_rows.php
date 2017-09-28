d2c37731d148672d073f5b39b2ae7a3a438b32d2
mbissett <czechdude489@gmail.com>
2017-09-28 00:25:47 -0500

    Create wp_all_import_skip_x_csv_rows.php

---
+++ all-import/wp_all_import_skip_x_csv_rows.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * ==========================================
+ * Filter: wp_all_import_skip_x_csv_rows
+ * ==========================================
+ *
+ * Specifies the amount of rows to be skipped.
+ *
+ * @since 4.4.4-beta-2.3
+ *
+ * @param $skip_rows   bool|int  - false to not skip rows, integer to specify how many rows should be skipped
+ * @param $import_id   int       - The ID of the current import
+ *
+ * @return $skip_rows
+ */
+
+add_filter('wp_all_import_skip_x_csv_rows', 'wpai_skip_rows', 10, 2);
+
+function wpai_skip_rows($skip_rows, $import_id) {
+    // Handle & return $skip_rows here.
+}
+
+// ----------------------------
+// Example uses below
+// ----------------------------
+/**
+ * Example: Skip the first row
+ *
+ */
+ 
+add_filter('wp_all_import_skip_x_csv_rows', 'wpai_skip_rows', 10, 2);
+
+function wpai_skip_rows($skip_rows, $import_id) {
+	$skip_rows = 1;
+
+	return $skip_rows;
+}
