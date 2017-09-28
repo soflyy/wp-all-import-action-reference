08be13bd6131d321d2498062558d0ca5ae5a0701
mbissett <czechdude489@gmail.com>
2017-09-28 02:19:17 -0500

    Added info about parameters & added example

--- all-export/pmxe_after_export.php
+++ all-export/pmxe_after_export.php
@@ -6,8 +6,8 @@
  * ======================================
  * Perform some action after the export is complete.
  *
- * @param $export_id
- * @param $exportObj
+ * @param $export_id   int     - The ID of the export that's running
+ * @param $exportObj   object  - Contains all of the data related to the export (export stats, export settings, the data being exported, etc.).
  *
  */
 function wp_all_export_after_export($export_id, $exportObj)
@@ -16,3 +16,26 @@ function wp_all_export_after_export($export_id, $exportObj)
     // if ($export_id === 5) {
 }
 add_action('pmxe_after_export', 'wp_all_export_after_export', 10, 2);
+
+// ----------------------------
+// Example uses below
+// ----------------------------
+
+/**
+ * Gets the export file once the export is complete
+ */
+
+add_action('pmxe_after_export', 'wpae_after_export', 10, 2);
+function wpae_get_file_after_export($export_id, $exportObj){
+	
+	// Check whether "Secure Mode" is enabled in All Export -> Settings
+	$is_secure_export = PMXE_Plugin::getInstance()->getOption('secure');
+	
+	if (!$is_secure_export) {
+		$filepath = get_attached_file($exportObj->attch_id);                    
+	} else {
+		$filepath = wp_all_export_get_absolute_path($exportObj->options['filepath']);
+	}
+	// Code to handle $filepath here
+	// e.g., rename( $filepath, "/path/to/new/folder/export-file-name.csv" );
+}
