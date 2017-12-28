a6c6b541f773c76b0c74655edcf8a7a7238a8b61
mbissett <czechdude489@gmail.com>
2017-12-28 17:56:53 -0600

    Update pmxe_after_export.php

--- all-export/pmxe_after_export.php
+++ all-export/pmxe_after_export.php
@@ -6,6 +6,9 @@
  * ======================================
  * Perform some action after the export is complete.
  *
+ * Note: In the case of this hook, the code needs to be added elsewhere.
+ * For example, your theme's functions.php file or a plugin like "Code Snippets".
+ *
  * @param $export_id   int     - The ID of the export that's running
  * @param $exportObj   object  - Contains all of the data related to the export (export stats, export settings, the data being exported, etc.).
  *
