f64dd14b799b660c405ab5cd812eb29534a9993f
Trey M <trey8611@gmail.com>
2017-12-28 17:17:51 -0500

    Added filter example for wp_all_export_xml_rows

--- all-export/wp_all_export_xml_rows.php
+++ all-export/wp_all_export_xml_rows.php
@@ -27,3 +27,35 @@ function my_wp_all_export_xml_rows($is_export_record, $record, $export_options, 
 
 add_filter('wp_all_export_xml_rows', 'my_wp_all_export_xml_rows', 10, 4);
 
+// ----------------------------
+// Example uses below
+// ----------------------------
+
+/**
+ * Export based on some criteria. In this case stock status or stock value in WooCommerce.
+ *
+ */
+function my_wp_all_export_xml_rows( $is_export_record, $record, $export_options, $export_id ) {
+    $is_export_record = true;
+	$product_id = $record->ID;
+	$stock_status = get_post_meta( $product_id, "_stock_status", true );
+	$stock = get_post_meta( $product_id, "_stock", true );
+	
+	// Now, we can conditionally export the records.
+	
+	// Example A: Don't export records that are out of stock:
+
+	// if ( $stock_status == 'outofstock' ) {
+	//  	$is_export_record = false;
+	// }
+	
+	// Example B: Only export records with a positive stock value:
+	
+	// if ( empty( $stock ) ) {
+	//  	$is_export_record = false;
+	// }
+	
+	// Now, we let WP All Export know whether we should export the record.
+    return $is_export_record;
+}
+add_filter( 'wp_all_export_xml_rows', 'my_wp_all_export_xml_rows', 10, 4 );
