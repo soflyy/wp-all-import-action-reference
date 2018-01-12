0b1090452aafac2385b37c25c646339bc1f4b531
Trey M <trey8611@gmail.com>
2018-01-12 14:29:42 -0500

    Fixed if condition example

--- all-import/pmxi_before_xml_import.php
+++ all-import/pmxi_before_xml_import.php
@@ -7,10 +7,9 @@
  * @param $import_id int - The import in progress
  *
  */
-function wp_all_import_before_xml_import($import_id)
-{
+function wp_all_import_before_xml_import( $import_id ) {
     // Unless you want this code to execute for every import, check the import id
-    // if ($import_id === 5) { ... }
+    // if ($import_id == 5) { ... }
 }
 
 add_action('pmxi_before_xml_import', 'wp_all_import_before_xml_import', 10, 1);
