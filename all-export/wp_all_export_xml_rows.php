c8a4821f4f0fe3eec2fb68cedd6d334ffaaa9e22
Programmer095 <Programmer095@gmail.com>
2018-05-06 14:44:41 -0500

    Add XML Export Record Limit Example

--- all-export/wp_all_export_xml_rows.php
+++ all-export/wp_all_export_xml_rows.php
@@ -59,3 +59,53 @@ function my_wp_all_export_xml_rows( $is_export_record, $record, $export_options,
     return $is_export_record;
 }
 add_filter( 'wp_all_export_xml_rows', 'my_wp_all_export_xml_rows', 10, 4 );
+
+/*
+ *
+ * Limit XML Export to a certain number of records
+ *
+ */
+
+add_filter('wp_all_export_xml_rows', function($shouldExport, $record, $exportOptions, $exportId) {
+
+    // THE ID OF THE EXPORT YOU WANT TO APPLY THE FILTER TO
+    $exportToApplyFilterTo = 3;
+
+    // THE NUMBER OF POSTS TO EXPORT
+    $lastNumberOfPostsToExport = 200;
+
+    if($exportId == $exportToApplyFilterTo) {
+
+        // GET THE LATEST POSTS
+        $posts = get_posts(
+            array(
+                'numberposts' => $lastNumberOfPostsToExport,
+                'post_status' => 'publish',  // ONLY RETURN PUBLISHED
+// START TAXONOMY QUERY, REMOVE IF NOT FILTERING BY TAXONOMY
+'tax_query' => array(
+    array(
+      'taxonomy' => 'property_status', // CHANGE TO THE TAXONOMY SLUG FOR YOUR TAXONOMY
+      'field' => 'term_id',
+      'terms' => 575, // CHANGE TO THE term_id OF YOUR TAXONOMY TERM THAT YOU WANT TO MATCH
+      'include_children' => false
+    )
+  ),
+// END TAXONOMY QUERY
+                'fields' => 'ids',
+                'order' => 'desc',
+                'orderby' => 'ID',
+                'post_type' => $record->post_type
+            )
+        );
+
+        // IF THE CURRENT POST'S ID WAS RETURNED BY THE QUERY ABOVE THEN EXPORT IT.
+        if(in_array($record->ID, $posts)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // RETURN THE DEFAULT IF EXPORT ID DOESN'T MATCH
+    return $shouldExport;
+}, 11, 4);
