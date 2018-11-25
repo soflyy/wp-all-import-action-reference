b1158414aaac9ac3d43e6c5baff5a51424147b0e
Soph Simmonds <soph@winusoft.co.uk>
2018-11-25 16:28:31 +0000

    Add example to add file extension to imported attachments

--- all-import/pmxi_attachment_uploaded.php
+++ all-import/pmxi_attachment_uploaded.php
@@ -20,4 +20,18 @@ function my_attachment_uploaded($post_id, $att_id, $file)
 add_action('pmxi_attachment_uploaded', 'my_attachment_uploaded', 10, 3);
 
 
-
+/**
+* Add attachment file extension
+*/
+function wpai_pmxi_attachment_uploaded( $pid, $attach_id, $attachment_file ) {
+	$new_file = $attachment_file . '.pdf';
+	if ( rename($attachment_file, $new_file) )
+	{
+		wp_update_post(array(
+			'ID' => $attach_id,			
+			'post_mime_type' => 'application/pdf'
+		));					
+		update_attached_file($attach_id,$new_file);		
+	}	
+}
+add_action('pmxi_attachment_uploaded', 'wpai_pmxi_attachment_uploaded', 10, 3);
