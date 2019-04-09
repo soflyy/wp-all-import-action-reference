c50d9f5207a926fbf4df9bed2a4e90560d3a753e
Maksym Tsypliakov <makstsiplyakov@gmail.com>
2019-04-09 23:31:28 +0200

    Updated pmxi_article_data filter

--- all-import/pmxi_article_data.php
+++ all-import/pmxi_article_data.php
@@ -11,10 +11,11 @@
  * @param $articleData    array  - TODO: Verify ... New content is in $articleData['post_content']
  * @param $import         object - TODO: Verify
  * @param $post_to_update object - Current content is in $post_to_update->post_content
+ * @param $current_xml_node array - Parsed data of current XML node
  *
  * @return mixed
  */
-function my_pmxi_article_data($articleData, $import, $post_to_update)
+function my_pmxi_article_data($articleData, $import, $post_to_update, $current_xml_node)
 {
     // Add new content to the top of old content
     // $articleData['post_content'] .= $post_to_update->post_content;
