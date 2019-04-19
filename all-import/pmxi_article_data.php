7d6aa2005354ff490fa51e8f68ffab5560f73b7a
Trey Mills <trey8611@gmail.com>
2019-04-19 08:10:13 -0400

    Update pmxi_article_data.php

--- all-import/pmxi_article_data.php
+++ all-import/pmxi_article_data.php
@@ -8,13 +8,14 @@
  *
  * This filter works only when 'Choose which data to update' options is chosen in import settings.
  *
- * @param $articleData    array  - TODO: Verify ... New content is in $articleData['post_content']
- * @param $import         object - TODO: Verify
- * @param $post_to_update object - Current content is in $post_to_update->post_content
+ * @param $articleData          array  - TODO: Verify ... New content is in $articleData['post_content']
+ * @param $import               object - TODO: Verify
+ * @param $post_to_update       object - Current content is in $post_to_update->post_content
+ * @param $current_xml_node     object - Current XML node
  *
  * @return mixed
  */
-function my_pmxi_article_data($articleData, $import, $post_to_update)
+function my_pmxi_article_data($articleData, $import, $post_to_update, $current_xml_node)
 {
     // Add new content to the top of old content
     // $articleData['post_content'] .= $post_to_update->post_content;
@@ -22,4 +23,4 @@ function my_pmxi_article_data($articleData, $import, $post_to_update)
     return $articleData;
 }
 
-add_filter('pmxi_article_data', 'my_pmxi_article_data', 10, 3);
+add_filter('pmxi_article_data', 'my_pmxi_article_data', 10, 4);
